4ae8a5ab0fec38694babf52ee5d178e06008b264
Codensity <codensity30@gmail.com>
2023-07-29 14:33:40 +0530

    1-5 problems on shortest path done

--- 13. Graphs/3. Topo Sort Problems/01. Topological Sorting.cpp
+++ 13. Graphs/3. Topo Sort Problems/01. Topological Sorting.cpp
@@ -6,21 +6,20 @@ APPROACH:
 - Topological sorting is a linear ordering of vertices in a directed acyclic graph (DAG) such that for every directed edge u -> v, vertex u comes before v in the ordering.
 - To find the topological sorting, we can use Depth-First Search (DFS) with backtracking.
 - We perform DFS starting from each unvisited node, and as we finish exploring a node and backtrack, we add it to the front of the topological sorting order.
-- We use a stack to keep track of the order of nodes in the topological sorting.
 
 COMPLEXITY ANALYSIS:
 - Time Complexity: O(V + E), where V is the number of vertices (nodes) and E is the number of edges in the graph. We visit each node and each edge exactly once during the DFS.
 - Space Complexity: O(V), where V is the number of vertices (nodes) in the graph. We use additional space to store the visited status of the nodes and the topological sorting order.
 */
 
-bool dfs(int node, vector<int> adj[], vector<bool>& vis, stack<int>& st){
+bool dfs(int node, vector<int> adj[], vector<bool>& vis, vector<int>& ans){
     vis[node] = true;
     for(auto v : adj[node]){
         if(!vis[v]){
             dfs(v, adj, vis, st);
         }
     }
-    st.push(node);
+    ans.push_back(node);
 }
 
 vector<int> topologicalSort(vector<vector<int>>& graph, int edges, int nodes) {
@@ -29,17 +28,14 @@ vector<int> topologicalSort(vector<vector<int>>& graph, int edges, int nodes) {
         adj[it[0]].push_back(it[1]);
     }
     vector<bool> vis(nodes);
-    stack<int> st;
+    vector<int> ans;
     for(int i = 0; i < nodes; i++){
         if(!vis[i]){
-            dfs(i, adj, vis, st);
+            dfs(i, adj, vis, ans);
         }
     }
 
-    vector<int> ans;
-    while(!st.empty()){
-        ans.push_back(st.top());
-        st.pop();
-    }
+    reverse(ans.begin(),ans.end());
+
     return ans;
 }
